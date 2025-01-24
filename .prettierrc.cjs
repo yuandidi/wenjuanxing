a8390cf84377579b0c993ac728055aab5f827fb3
yuandidi <2330919181@qq.com>
2025-01-24 11:43:47 +0800

    chore: init

---
+++ .prettierrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  //箭头函数只有一个参数时，是否要有小括号
+  arrowParens: 'avoid',
+  //括号内部不要出现空格
+  bracketSpacing: true,
+  //行结束符使用 Unix 格式
+  endOfLine: 'lf',
+
+  jsxBracketSameLine: false,
+  //行宽
+  printWidth: 100,
+  //换行方式
+  proseWrap: 'preserve',
+  //分号
+  semi: false,
+  //使用单引号
+  singleQuote: true,
+  //缩进
+  tabWidth: 2,
+  //使用tab缩进还是空格
+  useTabs: false,
+  //后置逗号，多行对象、数组在最后一项后面加逗号
+  trailingComma: 'es5',
+  parser: 'typescript',
+}
