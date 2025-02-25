2fa94d9e402dcca47d2bf83468f06455243135c2
yuandidi <2330919181@qq.com>
2025-02-25 18:01:18 +0800

    feature: 星标，复制，删除

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { Button, Divider, Space, Tag, Popconfirm, Modal, message } from 'antd'
 import {
@@ -10,11 +10,13 @@ import {
   ExclamationCircleOutlined,
 } from '@ant-design/icons'
 import styles from './QuestionCard.module.scss'
+import { useRequest } from 'ahooks'
+import { updateQuestionService, duplicateQuestionService } from '@/services/question'
 
 const { confirm } = Modal
 
 type PropsType = {
-  _id: number
+  _id: string
   title: string
   isStarred: boolean
   isPublished: boolean
@@ -26,24 +28,73 @@ const QuestionCard: FC<PropsType> = (props: PropsType) => {
   const nav = useNavigate()
   const { title, createdAt, answerCount, isPublished, _id, isStarred } = props
 
-  function duplicate() {
-    message.success('复制成功')
-  }
+  const [isStarredState, setIsStarredState] = useState(isStarred)
+  const [isDeletedState, setIsDeletedState] = useState(false)
+
+  const { loading: changeStarLoading, run: changeStar } = useRequest(
+    async () => {
+      await updateQuestionService(_id, { isStar: !isStarredState })
+    },
+    {
+      manual: true,
+      onSuccess() {
+        setIsStarredState(pre => !pre)
+        message.success('已设置')
+      },
+    }
+  )
+
+  //复制
+  const { loading: duplicateLoading, run: duplicate } = useRequest(
+    async () => {
+      const data = await duplicateQuestionService(_id)
+      return data
+    },
+    {
+      manual: true,
+      onSuccess(result) {
+        message.success('复制成功')
+        nav(`/question/edit/${result.id}`)
+      },
+    }
+  )
+
+  // function duplicate() {
+  //   message.success('复制成功')
+  // }
+
+  //删除
+  const { loading: deleteLoading, run: deleteQuestion } = useRequest(
+    async () => await updateQuestionService(_id, { isDeleted: true }),
+    {
+      manual: true,
+      onSuccess() {
+        message.success('删除成功')
+        setIsDeletedState(true)
+      },
+    }
+  )
 
   function del() {
     confirm({
       title: '确定删除该问卷？',
       icon: <ExclamationCircleOutlined />,
-      onOk: () => message.success('删除成功'),
+      onOk: () => deleteQuestion(),
     })
   }
+
+  //已经删除的问卷，不要再渲染卡片了
+  if (isDeletedState) {
+    return null
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <div className={styles.left}>
           <Link to={isPublished ? `/question/stat/${_id}` : `/question/edit/${_id}`}>
             <Space>
-              {isStarred && <StarOutlined style={{ color: 'red' }} />}
+              {isStarredState && <StarOutlined style={{ color: 'red' }} />}
               {title}
             </Space>
           </Link>
@@ -81,8 +132,14 @@ const QuestionCard: FC<PropsType> = (props: PropsType) => {
         </div>
         <div className={styles.right}>
           <Space>
-            <Button type="text" icon={<StarOutlined />} size="small">
-              {isStarred ? '取消星标' : '星标'}
+            <Button
+              type="text"
+              icon={<StarOutlined />}
+              size="small"
+              onClick={changeStar}
+              disabled={changeStarLoading}
+            >
+              {isStarredState ? '取消星标' : '星标'}
             </Button>
             <Popconfirm
               title="确定复制该问卷？"
@@ -90,11 +147,17 @@ const QuestionCard: FC<PropsType> = (props: PropsType) => {
               cancelText="取消"
               onConfirm={duplicate}
             >
-              <Button type="text" icon={<CopyOutlined />} size="small">
+              <Button type="text" icon={<CopyOutlined />} size="small" disabled={duplicateLoading}>
                 复制
               </Button>
             </Popconfirm>
-            <Button type="text" icon={<DeleteOutlined />} size="small" onClick={del}>
+            <Button
+              type="text"
+              icon={<DeleteOutlined />}
+              size="small"
+              onClick={del}
+              disabled={deleteLoading}
+            >
               删除
             </Button>
           </Space>
--- src/services/question.ts
+++ src/services/question.ts
@@ -30,3 +30,21 @@ export async function getQuestionListService(
   })) as ResDataType
   return data
 }
+
+//更新单个问卷
+export async function updateQuestionService(
+  id: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  opt: { [key: string]: any }
+): Promise<ResDataType> {
+  const url = `/api/question/${id}`
+  const data = (await axios.patch(url, opt)) as ResDataType
+  return data
+}
+
+//复制问卷
+export async function duplicateQuestionService(id: string): Promise<ResDataType> {
+  const url = `/api/question/duplicate/${id}`
+  const data = (await axios.post(url)) as ResDataType
+  return data
+}
